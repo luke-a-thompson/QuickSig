00df999c097d3ca379d6606bdf420fecbc3f4f29
Luke Thompson <luke-a-thompson@outlook.com>
2025-05-04 23:02:02 +1000

    Remove benchmark commit hook

--- run_check.sh
+++ run_check.sh
@@ -5,10 +5,6 @@ set -e
 pytest --quiet tests/test.py
 PYTEST_EXIT=$?
 
-# Run benchmark with regression checking
-python tests/benchmark.py --check-regression
-BENCHMARK_EXIT=$?
-
 # Exit with non-zero status if either command failed
 if [ $PYTEST_EXIT -ne 0 ] || [ $BENCHMARK_EXIT -ne 0 ]; then
     exit 1
